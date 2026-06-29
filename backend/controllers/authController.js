c2d7d9da8ab5c17758235eae40e36c1170a1524f
Yashika Sinha <yashikasinha309@gmail.com>
2026-06-29 18:05:27 +0530

    Implemented bcrypt for password safety

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,12 +1,24 @@
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    return res.status(400).json({
+      success: false,
+      message: "User already exists",
+    });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
   const user = await User.create({
     name,
     email,
-    password,
+    password: hashedPassword,
   });
 
   res.status(201).json({
@@ -16,4 +28,35 @@ const registerUser = async (req, res) => {
   });
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid Email or Password",
+    });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid Email or Password",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Login Successful",
+    user,
+  });
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
